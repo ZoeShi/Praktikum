91701ee54bc21dbb52164263175c129f2347621a
zoe <zoeshi9@googlemail.com>
2017-12-13 08:48:13 +0100

    Update rsa2.py

--- rsa2.py
+++ rsa2.py
@@ -3,7 +3,7 @@ while True:
     x = input('gebe eine zahl ein:')
     e = input('gebe e ein:')
     d = input('gebe d ein:')
-    = input('gebe N ein:')
+    N = input('gebe N ein:')
     c = x**e % N
     print(x**e % N)
 
